6d01a125f867425ce60448647a10bf6e8b9f1e85
lancewuz <lancewuz@163.com>
2020-03-04 19:48:16 +0800

    chore: add coverage thresholds

--- nyc.config.js
+++ nyc.config.js
@@ -3,4 +3,11 @@ module.exports = {
   // https://istanbul.js.org/docs/advanced/alternative-reporters/#cobertura
   reporter: ['cobertura', 'text'],
   include: ['src/*.js', 'index.js'],
+  'check-coverage': true,
+
+  // https://github.com/istanbuljs/nyc#coverage-thresholds
+  branches: 100,
+  lines: 100,
+  functions: 95,
+  statements: 100,
 };
